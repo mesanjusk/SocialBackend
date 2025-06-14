682dc8ebd4a872909bc8e1605b39ad8a27706cd6
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-06-14 21:56:34 +0530

    Update Admission.js

--- models/Admission.js
+++ models/Admission.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const admissionSchema = new mongoose.Schema({
+  uuid: { type: String, default: uuidv4 },
   branchCode: String,
   admissionDate: Date,
   firstName: String,
