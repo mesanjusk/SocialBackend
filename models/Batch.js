f3695d199e44cdecd9c50cf8c92cbebea7a6cc73
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-06-15 20:31:50 +0530

    Update Batch.js

--- models/Batch.js
+++ models/Batch.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const batchSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  timing: { type: String }
+  timing: { type: String },
+  organization_id: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Batch', batchSchema);
