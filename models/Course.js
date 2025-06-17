19f7a5c9bd72d81a30398323d9ab1be59221e02b
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-06-17 21:34:08 +0530

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const courseSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String },
-  organization_id: { type: String, required: true }
+  uuid: {
+    type: String,
+    default: uuidv4,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: String,
+  courseFees: String,
+  examFees: String,
+  duration: String,
+  organization_id: {
+    type: String,
+    required: true
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Course', courseSchema);
