7dd368cf9b8267a7ecd001457b892440b39723a5
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-06-17 20:42:45 +0530

    Update OrgCategory.js

--- models/OrgCategory.js
+++ models/OrgCategory.js
@@ -1,15 +1,18 @@
+const { v4: uuidv4 } = require('uuid');
 const mongoose = require('mongoose');
 
 const orgCategorySchema = new mongoose.Schema({
+  uuid: {
+    type: String,
+    default: uuidv4,
+    unique: true,
+    required: true,
+  },
   category: {
     type: String,
     required: true,
-    trim: true
   },
-  description: {
-    type: String,
-    trim: true
-  }
+  description: String,
 }, { timestamps: true });
 
 module.exports = mongoose.model('OrgCategory', orgCategorySchema);
