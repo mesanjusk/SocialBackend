a37214e71d6839c674a17de1bf2d421610bf4788
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-06-17 20:44:56 +0530

    Update PaymentMode.js

--- models/PaymentMode.js
+++ models/PaymentMode.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const paymentModeSchema = new mongoose.Schema({
+  uuid: {
+    type: String,
+    default: uuidv4,
+    required: true,
+    unique: true,
+  },
   mode: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   description: {
     type: String,
-    trim: true
-  }
+    trim: true,
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model('PaymentMode', paymentModeSchema);
