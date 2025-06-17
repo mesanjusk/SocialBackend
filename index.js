d8d107e50ca525f2f31cfe4fc5118281bd60535e
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-06-17 10:26:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use('/api/batches', batchRoutes);
 app.use('/api/org-categories', orgCategoryRoutes);
 app.use('/api/education', educationRoutes);
 app.use('/api/exams', examRoutes);
-app.use('/api/paymentmodes', paymentModeRoutes);
+app.use('/api/paymentmode', paymentModeRoutes);
 
 // 404 fallback
 app.use((req, res) => {
